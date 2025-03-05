0ec5bf262ea3d5432a21723db73dc6000c550dd7
SmiygohelDarshan <240101010090darshan.ac.in>
2025-03-05 20:16:19 +0530

    add Extra Program

---
+++ 34.c
@@ -0,0 +1,17 @@
+// WAP to convert an Octal into hexa-decimal.
+#include <stdio.h>
+#include <math.h>
+void main(){
+    int octal, decimal = 0, position = 0, rem;
+
+    printf("Enter a Octal number: ");
+    scanf("%d", &octal);
+
+    while (octal > 0) {
+        rem = octal % 10;  
+        decimal += rem * pow(8, position);  
+        octal /= 10;  
+        position++;  
+    }
+    printf("Decimal: %d", decimal);
+}
